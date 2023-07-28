286f6110da40e0b6e16ef3727266d6e80dfc7798
Vitor Lopes <vitoraugustoa@hotmail.com>
2023-07-27 21:14:44 -0300

    Atualizando.

---
+++ aulas/01-fundamentos-reactjs/src/components/Header.tsx
@@ -0,0 +1,10 @@
+import igniteLogo from '../assets/ignite-logo.svg';
+import styles from './Header.module.css';
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={igniteLogo} alt="Logotipo do Ignite" />
+    </header>
+  );
+}
