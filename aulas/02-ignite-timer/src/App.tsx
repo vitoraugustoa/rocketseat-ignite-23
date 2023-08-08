f3e06d255b245b2bcbe4ab381ce5d573a5d3ed32
Vitor Lopes <vitoraugustoa@hotmail.com>
2023-08-08 20:28:38 -0300

    Atualizando.

--- aulas/02-ignite-timer/src/App.tsx
+++ aulas/02-ignite-timer/src/App.tsx
@@ -1,6 +1,20 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { Button } from "./components/Button";
+import { defaultTheme } from "./styles/themes/default";
+
 export function App() {
 
   return (
-   <h1>hello world</h1>
+    <>
+      <ThemeProvider theme={defaultTheme}>
+        <Button variant="primary" />
+        <Button variant="secondary" />
+        <Button variant="success" />
+        <Button variant="danger" />
+        <Button />
+        <GlobalStyle />
+      </ThemeProvider >
+    </>
   )
 }
