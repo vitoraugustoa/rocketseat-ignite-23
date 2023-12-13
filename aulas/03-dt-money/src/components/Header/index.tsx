03dbeaba246531c3b9af4b351781f0701ede2c5d
vitor.alopes <vitor.alopes@agnet.com.br>
2023-12-12 21:06:57 -0300

    Atualizando.

--- aulas/03-dt-money/src/components/Header/index.tsx
+++ aulas/03-dt-money/src/components/Header/index.tsx
@@ -1,4 +1,6 @@
-import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import { HeaderContainer, HeaderContent, NewTransactionButton} from "./styles";
+import { NewTransactionModal } from "../NewTransactionModa";
+import * as Dialog from '@radix-ui/react-dialog';
 
 import logoImg from "../../assets/logo.svg";
 
@@ -8,7 +10,13 @@ export function Header() {
       <HeaderContent>
         <img src={logoImg} alt="" />
 
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+        <Dialog.Trigger asChild>
+          <NewTransactionButton>Nova transação</NewTransactionButton>
+        </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
