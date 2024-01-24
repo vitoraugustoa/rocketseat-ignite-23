d42bf8963c0e9452725d59d1edf014d180d699a9
vitor.alopes <vitor.alopes@agnet.com.br>
2024-01-23 21:26:50 -0300

    Atualizando.

---
+++ aulas/04-pizza-shop-web/readme.txt
@@ -0,0 +1,16 @@
+Projeto utiliza:
+
+Component library: https://ui.shadcn.com/
+Que utiliza por trás dos panos: www.radix-ui.com
+
+Para trabalhar com formulários: React Hook Form https://react-hook-form.com/
+Para trabalhar com validação de formulário: https://zod.dev/
+E o @hookform/resolvers para integrar zod com react hook form
+
+Biblioteca para toast: https://sonner.emilkowal.ski/
+
+Para trabalhar com Icones: https://lucide.dev
+
+Para trabalhar com titulos da pagina: https://github.com/staylor/react-helmet-async
+
+Para trabalhar com gráficos: https://recharts.org/en-US/
--- aulas/04-pizza-shop-web/src/App.tsx
+++ aulas/04-pizza-shop-web/src/App.tsx
@@ -3,13 +3,19 @@ import './global.css'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
+import { Toaster } from 'sonner'
+import { ThemeProvider } from './components/theme/theme-provider'
 
 function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
+      <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
+        <Helmet titleTemplate="%s | pizza.shop" />
 
-      <RouterProvider router={router} />
+        <Toaster richColors />
+
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
