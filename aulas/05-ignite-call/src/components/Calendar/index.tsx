e1362871289bafc09bc603d7594f9637b1c1e2bc
vitor.alopes <vitor.alopes@agnet.com.br>
2024-04-19 09:40:16 -0300

    Atualizando.

--- aulas/05-ignite-call/src/components/Calendar/index.tsx
+++ aulas/05-ignite-call/src/components/Calendar/index.tsx
@@ -26,6 +26,7 @@ type CalendarWeeks = CalendarWeek[]
 
 interface BlockedDates {
   blockedWeekDays: number[]
+  blockedDates: number[]
 }
 
 interface CalendarProps {
@@ -65,7 +66,7 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
       const response = await api.get(`/users/${username}/blocked-dates`, {
         params: {
           year: currentDate.get('year'),
-          month: currentDate.get('month'),
+          month: currentDate.get('month') + 1,
         },
       })
 
@@ -74,6 +75,12 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
 })
 
   const calendarWeeks = useMemo(() => {
+    if (!blockedDates) {
+      return []
+    }
+
+    console.log('calendarWeeks ~ blockedDates', blockedDates)
+
     const daysInMonthArray = Array.from({
       length: currentDate.daysInMonth(),
     }).map((_, i) => {
@@ -111,7 +118,8 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
           date,
           disabled:
             date.endOf('day').isBefore(new Date()) ||
-            blockedDates?.blockedWeekDays.includes(date.get('day')),
+            blockedDates?.blockedWeekDays.includes(date.get('day')) ||
+            blockedDates?.blockedDates.includes(date.get('date')),
         }
       }),
       ...nextMonthFillArray.map((date) => {
