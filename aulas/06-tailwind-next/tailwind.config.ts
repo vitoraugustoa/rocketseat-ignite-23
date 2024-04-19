a1c4822e6c25623c94b4a1fc4a4160fbb2a3bef8
vitor.alopes <vitor.alopes@agnet.com.br>
2024-04-19 11:44:11 -0300

    Atualizando.

---
+++ aulas/06-tailwind-next/tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [ "./src/**/*.tsx" ],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        app: 'minmax(18rem, 20rem) 1fr',
+      },
+      maxWidth: {
+        app: '700px'
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
