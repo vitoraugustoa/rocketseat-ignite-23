9ccc3419ab9309d7c9bfb76758f83cbb4fc62440
vitor.alopes <vitor.alopes@agnet.com.br>
2024-05-07 17:05:48 -0300

    Finalizando.

--- aulas/07-next-app-router/src/app/(store)/product/[slug]/page.tsx
+++ aulas/07-next-app-router/src/app/(store)/product/[slug]/page.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
-
+import { Metadata } from "next";
 import { api } from "@/data/api";
 import { Product } from "@/data/types/product";
+import { AddToCartButton } from "@/components/add-to-cart-button";
 
 interface ProductProps {
   params: {
@@ -19,6 +20,25 @@ async function getProduct(slug: string): Promise<Product> {
   const product = await response.json();
 
   return product;
+}
+
+export async function generateMetadata({
+  params,
+}: ProductProps): Promise<Metadata> {
+  const product = await getProduct(params.slug);
+
+  return {
+    title: product.title,
+  };
+}
+
+export async function generateStaticParams() {
+  const response = await api("/products/featured");
+  const products: Product[] = await response.json();
+
+  return products.map((product) => {
+    return { slug: product.slug };
+  });
 }
 
 export default async function ProductPage({ params }: ProductProps) {
@@ -45,6 +65,7 @@ export default async function ProductPage({ params }: ProductProps) {
 
         <div className="mt-8 flex items-center gap-3">
           <span className="inline-block rounded-full bg-violet-500 px-5 py-2.5 font-semibold">
+            Em até 12x s/ juros de{" "}
             {product.price.toLocaleString("pt-BR", {
               style: "currency",
               currency: "BRL",
@@ -91,12 +112,7 @@ export default async function ProductPage({ params }: ProductProps) {
           </div>
         </div>
 
-        <button
-          type="button"
-          className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold text-white"
-        >
-          Adicionar ao carrinho
-        </button>
+        <AddToCartButton productId={product.id} />
       </div>
     </div>
   );
--- aulas/07-next-app-router/src/app/(store)/search/loading.tsx
+++ aulas/07-next-app-router/src/app/(store)/search/loading.tsx
@@ -1,3 +1,22 @@
+import { Suspense } from 'react'
+import { Skeleton } from "@/components/skeleton";
+import { CurrentSearch } from './current-search'
+
 export default function SearchLoading() {
-  return <p>Carregando busca...</p>
+  return (
+    <div className="flex flex-col gap-4">
+       <Suspense fallback={null}>
+        <CurrentSearch />
+      </Suspense>
+
+      <div className="grid grid-cols-3 gap-6">
+        <Skeleton className="h-[400px]" />
+        <Skeleton className="h-[400px]" />
+        <Skeleton className="h-[400px]" />
+        <Skeleton className="h-[400px]" />
+        <Skeleton className="h-[400px]" />
+        <Skeleton className="h-[400px]" />
+      </div>
+    </div>
+  );
 }
